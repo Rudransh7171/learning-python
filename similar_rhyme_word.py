b59d7b9b13bea255b79112be6eb23d89316feced
Rudransh7171 <58659907+Rudransh7171@users.noreply.github.com>
2020-07-06 23:26:24 +0530

    changed the format of veiwing data

--- similar_rhyme_word.py
+++ similar_rhyme_word.py
@@ -1,9 +1,10 @@
 import requests
 import json
-
 c = str(input("similar word for?"))
 b_url = 'https://api.datamuse.com/words'
 d = {'rel_rhy':c}
 req = requests.get(b_url,params=d)
 x = req.json()
-print([d['word'] for d in x])
+e = [d['word'] for d in x]
+for i in e:
+    print('->',i)
